cf96f5581a8a261a15b500005d87998d826a2bd0
ekdnlt <ekdnlt1532@naver.com>
2021-06-03 16:04:26 +0900

    Multi-threading1

--- app/src/main/java/com/example/threadbasic/MainActivity.java
+++ app/src/main/java/com/example/threadbasic/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         running = true;
+
         wt= new WorkerThread();
         wr = new Thread(new WorkerRunnable());
 
